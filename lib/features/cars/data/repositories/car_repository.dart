c395b91d29a5aec888351e1eed67129424ddf98f
Amos Godwin <amosgodwin500@gmail.com>
2024-02-02 17:03:13 +0100

    wrote repo methods to fetch from all endpoints

---
+++ lib/features/cars/data/repositories/car_repository.dart
@@ -0,0 +1,80 @@
+import 'package:autochek_assessment/features/cars/data/models/car_inventory.dart';
+import 'package:autochek_assessment/features/cars/data/models/car_inventory_detail.dart';
+import 'package:autochek_assessment/features/cars/data/models/car_make.dart';
+import 'package:autochek_assessment/features/cars/data/models/car_media.dart';
+import 'package:autochek_assessment/utils/api_config.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class CarRepository {
+
+  CarRepository({required this.dio})
+      : _dio = Dio(
+    BaseOptions(
+      connectTimeout: const Duration(seconds: 3),
+      receiveTimeout: const Duration(seconds: 3),
+    ),
+  );
+
+  final Dio dio;
+
+  final Dio _dio;
+
+
+  Future<Either<String, CarMakes>>  fetchCarPopularMakes() async {
+    try {
+      final response = await _dio.get(ApiConfig.popularMakes,);
+      if (response.statusCode == 200 && response.data != null) {
+        final carMakes = CarMakes.fromJson(response.data!,);
+        return Right(carMakes);
+      } else {
+        return Left(response.statusMessage ?? 'Error');
+      }
+    } on DioException catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, CarInventory>>  fetchCarInventory() async {
+    try {
+      final response = await _dio.get(ApiConfig.allCars,);
+      if (response.statusCode == 200 && response.data != null) {
+        final carMakes = CarInventory.fromJson(response.data!,);
+        return Right(carMakes);
+      } else {
+        return Left(response.statusMessage ?? 'Error');
+      }
+    } on DioException catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, CarInventoryDetail>>  fetchCarInventoryDetail({required String carId}) async {
+    try {
+      final response = await _dio.get(ApiConfig.carDetail(carId: carId),);
+      if (response.statusCode == 200 && response.data != null) {
+        final carMakes = CarInventoryDetail.fromJson(response.data!,);
+        return Right(carMakes);
+      } else {
+        return Left(response.statusMessage ?? 'Error');
+      }
+    } on DioException catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, CarMedia>>  fetchCarMedia({required String carId}) async {
+    try {
+      final response = await _dio.get(ApiConfig.carMedia(carId: carId),);
+      if (response.statusCode == 200 && response.data != null) {
+        final carMakes = CarMedia.fromJson(response.data!,);
+        return Right(carMakes);
+      } else {
+        return Left(response.statusMessage ?? 'Error');
+      }
+    } on DioException catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+}
---
+++ lib/utils/api_config.dart
@@ -0,0 +1,9 @@
+
+abstract class ApiConfig {
+  static String baseUrl = ' https://api.staging.myautochek.com/v1/inventory';
+
+  static String popularMakes = '$baseUrl/make?popular=true';
+  static String allCars = '$baseUrl/car/search';
+  static String carDetail({required String carId}) => '$baseUrl/car/$carId';
+  static String carMedia({required String carId}) => '$baseUrl/car_media?carId=$carId';
+}
